449bb7fa93ce5667bdbce0ef24fdf2491eebb435
unknown <bousilmustapha@gmail.com>
2022-12-14 21:57:25 +0100

    install setup

---
+++ ecommerce/app/Http/Livewire/Admin/AdminDashboardComponent/Blade/Php.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\AdminDashboardComponent\Blade;
+
+use Livewire\Component;
+
+class Php extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.admin-dashboard-component.blade.php');
+    }
+}
---
+++ ecommerce/app/Http/Livewire/User/UserDashboardComponent/Blade/Php.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\UserDashboardComponent\Blade;
+
+use Livewire\Component;
+
+class Php extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.user-dashboard-component.blade.php');
+    }
+}
