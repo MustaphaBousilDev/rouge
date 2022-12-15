29b81c02d22d087ef928a7bb888a5d1320ccf9d9
unknown <bousilmustapha@gmail.com>
2022-12-15 19:41:39 +0100

    work front end

---
+++ front-end/assets/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['../*.html'],
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      current:'currentColor',
+      'while':'#fff',
+      'color-gray-dark':'#262626',
+      'color-red-button':'#ef4444',
+      'color-gray-background-light':'#d4d4d8',
+      'color-rating':'#f59e0b'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
