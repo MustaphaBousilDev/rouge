f29c94e37c3859ef2d6f182fef3bcda6eaa49ebb
unknown <bousilmustapha@gmail.com>
2023-02-15 11:08:15 +0100

    add product review description

--- front-end/assets/js/product-details/dist/slider.dev.js
+++ front-end/assets/js/product-details/dist/slider.dev.js
@@ -3,10 +3,7 @@
 var right_icon_details = document.querySelector('.product__details-page .products__details .product__principalte-details .right-icon__product-details');
 var left_icon_details = document.querySelector('.product__details-page .products__details .product__principalte-details .left-icon__product-details');
 var img_principal_details = document.querySelector('.product__details-page .products__details .product__principalte-details img');
-console.log(img_principal_details);
 var imgs_all_details = document.querySelectorAll('.product__details-page .products__details .products__secondaires-details .product__secondary-detail img');
-console.log(imgs_all_details);
-console.log('fuck');
 var counter_product_details = 0;
 imgs_all_details[counter_product_details].classList.add('active');
 right_icon_details.addEventListener('click', function () {
@@ -38,4 +35,17 @@ left_icon_details.addEventListener('click', function () {
     imgs_all_details[counter_product_details].classList.add('active');
     img_principal_details.src = imgs_all_details[counter_product_details].src;
   }
+});
+imgs_all_details.forEach(function (img) {
+  img.addEventListener('click', function () {
+    imgs_all_details.forEach(function (img) {
+      img.classList.remove('active');
+    });
+    img.classList.add('active');
+    img_principal_details.src = img.src;
+    var ind = parseInt(img.dataset.index);
+    console.log('totot');
+    console.log(ind);
+    counter_product_details = ind;
+  });
 });
--- front-end/assets/js/product-details/slider.js
+++ front-end/assets/js/product-details/slider.js
@@ -1,10 +1,9 @@
 let right_icon_details=document.querySelector('.product__details-page .products__details .product__principalte-details .right-icon__product-details')
 let left_icon_details=document.querySelector('.product__details-page .products__details .product__principalte-details .left-icon__product-details')
 let img_principal_details=document.querySelector('.product__details-page .products__details .product__principalte-details img')
-console.log(img_principal_details)
+
 let imgs_all_details=document.querySelectorAll('.product__details-page .products__details .products__secondaires-details .product__secondary-detail img')
-console.log(imgs_all_details)
-console.log('fuck')
+
 let counter_product_details=0;
 imgs_all_details[counter_product_details].classList.add('active')
 
@@ -40,4 +39,19 @@ left_icon_details.addEventListener('click',function(){
         imgs_all_details[counter_product_details].classList.add('active')
         img_principal_details.src=imgs_all_details[counter_product_details].src
     }
+})
+
+imgs_all_details.forEach((img)=>{
+
+    img.addEventListener('click',function(){
+        imgs_all_details.forEach((img)=>{
+            img.classList.remove('active')
+        })
+        img.classList.add('active')
+        img_principal_details.src=img.src 
+        let ind=parseInt(img.dataset.index)
+        console.log('totot')
+        console.log(ind)
+        counter_product_details=ind 
+    })
 })
